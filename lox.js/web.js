7db15e83ecab0e0865bada2b6e1d7870268b55a4
yakoilser-netizen <yakoilser@gmail.com>
2026-02-17 16:44:04 +0300

    Add files via upload

---
+++ lox.js/web.js
@@ -0,0 +1,7 @@
+// Вся клиентская логика здесь!
+console.log("Успешно");
+
+// Меняем цвета
+//document.querySelector('h1').style.color = 'red';
+//document.querySelector('h2').style.color = 'blue';
+
